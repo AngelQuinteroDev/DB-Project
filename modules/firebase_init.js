7c89881ac0b0730368a90d8de59c788b174d7e8c
Angel <angelquintero.dev@gmail.com>
2025-04-23 20:08:39 -0500

    Config Firebase Init

--- modules/firebase_init.js
+++ modules/firebase_init.js
@@ -5,14 +5,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCAoqk0ECwUOxqtDqy6Uvnvn2P33Vao6us",
-  authDomain: "zoologico-f93b7.firebaseapp.com",
-  projectId: "zoologico-f93b7",
-  storageBucket: "zoologico-f93b7.firebasestorage.app",
-  messagingSenderId: "650624564645",
-  appId: "1:650624564645:web:3345ec308271ed200abc4c",
-  measurementId: "G-HS11Y7WYKE"
+  apiKey: "AIzaSyAgVj5FAzSRPn_7n-ZX1joUEcKrta0VCiM",
+  authDomain: "superzoo-4d820.firebaseapp.com",
+  projectId: "superzoo-4d820",
+  storageBucket: "superzoo-4d820.firebasestorage.app",
+  messagingSenderId: "82513931469",
+  appId: "1:82513931469:web:dc06274bb62a45eacd4d84"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
